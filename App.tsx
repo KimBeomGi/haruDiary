179425357e9a95f47ef08055b61a8797140bcb25
kimbeomgi <dyd1308@naver.com>
2025-02-08 22:39:56 +0900

    react native와 firebase 연결결

--- App.tsx
+++ App.tsx
@@ -31,9 +31,6 @@ import { store } from './store/store'
 import { Provider } from 'react-redux'
 
 import HomeScreen from './src/screens/HomeScreen'
-import TestScreen1 from './src/screens/TestScreen1';
-import TestScreen2 from './src/screens/TestScreen2';
-import TestScreen3 from './src/screens/TestScreen2';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
