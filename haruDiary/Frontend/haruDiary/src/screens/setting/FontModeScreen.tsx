c40bbe1d181a1ec579eb86bc3e426701134d3ea6
kimbeomgi <dyd1308@naver.com>
2025-03-21 15:01:37 +0900

    "폰트 라디오 버튼 최적화"

--- haruDiary/Frontend/haruDiary/src/screens/setting/FontModeScreen.tsx
+++ haruDiary/Frontend/haruDiary/src/screens/setting/FontModeScreen.tsx
@@ -30,9 +30,20 @@ function FontModeScreen(): React.JSX.Element {
   const [isCheck, setIsCheck] = useState(false)
   const fontfamily = useSelector((state: RootState) => state.font.fontFamily)
   const [preparedFonts, setPreparedFonts] = useState([
-    "NanumMyeongjo", "NanumGoRyeoGeurGgor", "SunBatang-Medium", "KimKongHae", "ANDONG 264 TTF", "GodoM"
+    ["나눔명조", "NanumMyeongjo"], ["나눔손글씨 고려글꼴","NanumGoRyeoGeurGgor"], ["순바탕", "SunBatang-Medium"], 
+    ["온글잎 김콩해", "KimKongHae"], ["안동 이육사체", "ANDONG 264 TTF"], ["고도체", "GodoM"]
   ])
   const [lorem, setLorem] = useState("우는 날들을 이런 없이 향할 회한도 걸 왔을까? 그 마른 동산에 너무나 하나였던 보내 시각에 타는 때. 건너온 기억해주오 편지도 하늘을 불러주던 밤을 잎들은 위에도 말했다. \n\n목란배 내지 번을 했던 아무 이네들은 있다. 부드럽게, 나는 별에도 생을 아무것도 이름자를 자신을 청명한 하나였던 이름과, 지우지 꽃을 생명을 가을 말없이 말 오매불망 아니라 했다.")
+
+  // 라디오 버튼에 들어갈 내용들을 만들어주는 함수
+  const getRadioBtnNames = (fonts: string[][]): { textName: string; textFont: string; iconName: null; selected: number }[] => {
+    return fonts.map((font, index) => ({
+      textName: font[0],
+      textFont: font[1],
+      iconName: null,
+      selected: index,
+    }));
+  };
 
   const handleSelect = (textName: string, selectedIndex: number) => {
     setSelectedFont(textName);
@@ -40,12 +51,7 @@ function FontModeScreen(): React.JSX.Element {
     // // 선택된 모드에 따라 필요한 작업 수행
     whatFontSet(textName, selectedIndex)
     // // store에 작업
-    let fontFamily: 'basic' | '1st' | '2nd' | '3rd' | '4th' = 'basic';
-    if (selectedIndex === 0) fontFamily = 'basic';
-    if (selectedIndex === 1) fontFamily = '1st';
-    if (selectedIndex === 2) fontFamily = '2nd';
-    if (selectedIndex === 3) fontFamily = '3rd';
-    if (selectedIndex === 4) fontFamily = '4th';
+    let fontFamily = preparedFonts[selectedIndex][1]
     dispatch(setFontFamily(fontFamily));
   };
 
@@ -58,7 +64,7 @@ function FontModeScreen(): React.JSX.Element {
         if (value) {
           setSelectedFont(value.textName);
           setSelected(value.selectedIndex);
-          dispatch(setFontFamily(preparedFonts[value.selectedIndex]))
+          dispatch(setFontFamily(preparedFonts[value.selectedIndex][1]))
           setIsCheck(true)  // value가 들어온 후에 렌더링하기 위함.
         }
       };
@@ -109,19 +115,12 @@ function FontModeScreen(): React.JSX.Element {
         </View>
         {isCheck && (
           <RadioBtnGroup
-            names={[
-              {textName :'나눔명조', textFont : "NanumMyeongjo",iconName : null, selected : 0},
-              {textName :'나눔손글씨 고려글꼴', textFont : "NanumGoRyeoGeurGgor",iconName : null, selected : 1},
-              {textName :'순바탕', textFont : "SunBatang-Medium",iconName : null, selected : 2},
-              {textName :'온글잎 김콩해', textFont : "KimKongHae",iconName : null, selected : 3},
-              {textName :'안동 이육사체', textFont : "ANDONG 264 TTF",iconName : null, selected : 4},
-              {textName :'고도체', textFont : "GodoM",iconName : null, selected : 5},
-            ]}
+            names={getRadioBtnNames(preparedFonts)}
             onSelect={handleSelect} // 콜백 함수 전달
             selected={selected} // 선택된 라디오 버튼 인덱스 전달
           />
         )}
-        {/* <Text>{fontfamily}</Text> */}
+        <Text>{fontfamily}</Text>
         
       </ScrollView>
     </SafeAreaView>
