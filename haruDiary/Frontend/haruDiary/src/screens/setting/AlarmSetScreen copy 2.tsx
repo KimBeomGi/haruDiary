890f990b16c87bbfcf800f04728666b938eceb3f
kimbeomgi <dyd1308@naver.com>
2025-03-23 21:03:20 +0900

    "앱 알림기능 적용용"

--- haruDiary/Frontend/haruDiary/src/screens/setting/AlarmSetScreen copy 2.tsx
+++ haruDiary/Frontend/haruDiary/src/screens/setting/AlarmSetScreen copy 2.tsx
@@ -1,5 +1,5 @@
 import { useFocusEffect, useNavigation } from '@react-navigation/native';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView, Text, View, Button, StyleSheet, ScrollView, TouchableOpacity, Switch } from 'react-native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../navigations/AppNavigator'; // RootStackParamList를 가져옵니다.
@@ -12,7 +12,7 @@ import AlarmSetModal from '../../modals/AlarmSetModal';
 import { selectTab } from '../../../store/bottom/bottomTabSlice';
 import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker'; // 알림 시간 조정에 쓰임임
 import { whenAlarmTimeGet, whenAlarmTimeSet } from '../../asyncStorage/asyncStorage';
-import notifee, { AndroidColor, AndroidImportance, RepeatFrequency, TimestampTrigger, TriggerType } from '@notifee/react-native';
+import notifee, { AndroidColor } from '@notifee/react-native';
 
 
 type AlarmSetScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'AlarmSet'>;
@@ -48,102 +48,12 @@ function AlarmSetScreen(): React.JSX.Element {
       setTime(currentDate.toTimeString().slice(0,5))
       whenAlarmTimeSet(selectedDate.getTime(), isAlarmEnabled)// 선택한 Date getTime으로 unix 타임스탬프로 변환후 저장하기
     }
-    if(isAlarmEnabled){
-      scheduleAlarm()
-    }
   };
   
   const handleIsAlarmEnabled = () => {
     const tmp = !isAlarmEnabled
-    if(!tmp){
-      cancelAlarm()
-    }else{
-      scheduleAlarm()
-    }
     setIsAlarmEnabled(tmp)
     whenAlarmTimeSet(date.getTime(), tmp)
-  }
-
-  useEffect(() => {
-    async function createChannel() {
-      await notifee.createChannel({
-        id: 'alarm_channel',
-        name: 'Alarm Channel',
-        importance: AndroidImportance.HIGH,
-        vibration: true,
-      });
-    }
-    createChannel();
-  }, []);
-
-  useEffect(() => {
-    if (isAlarmEnabled) {
-      scheduleAlarm();
-    } else {
-      cancelAlarm();
-    }
-  }, [isAlarmEnabled, date]);
-  
-  // async function onDisplayNotification() {
-  //   // Request permissions (required for iOS)
-  //   await notifee.requestPermission()
-
-  //   // Create a channel (required for Android)
-  //   const channelId = await notifee.createChannel({
-  //     id: 'alarm_Channel',
-  //     name: 'Alarm Channel',
-  //   });
-
-  //   // Display a notification
-  //   await notifee.displayNotification({
-  //     title: '일기 작성 시간!',
-  //     body: '하루일기에서 오늘의 이야기를 작성해보세요.',
-  //     android: {
-  //       channelId : 'alarm_Channel',
-  //       // smallIcon: 'name-of-a-small-icon', // optional, defaults to 'ic_launcher'.
-  //       // pressAction is needed if you want the notification to open the app when pressed
-  //       pressAction: {
-  //         id: 'default',
-  //       },
-  //     },
-  //   });
-  // }
-
-  async function scheduleAlarm() {
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const now = new Date();
-    const alarmTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hours, minutes);
-
-    if (alarmTime <= now) {
-      alarmTime.setDate(alarmTime.getDate() + 1);
-    }
-
-    const trigger: TimestampTrigger = {
-      type: TriggerType.TIMESTAMP,
-      timestamp: alarmTime.getTime(),
-    };
-
-    const notification = {
-      title: '일기 작성 시간!',
-      body: `설정하신 ${hours}:${minutes}에 알람이 울립니다. \n 하루일기에서 오늘의 이야기를 작성해보세요.`,
-      android: {
-        channelId: 'alarm_channel',
-        pressAction: {
-          id: 'default',
-        },
-      },
-    };
-
-    const triggerId = await notifee.createTriggerNotification(notification, trigger);
-    setTriggerId(triggerId);
-  }
-
-  async function cancelAlarm() {
-    if (triggerId) {
-      await notifee.cancelTriggerNotification(triggerId);
-      setTriggerId(null);
-    }
   }
 
 
@@ -163,9 +73,6 @@ function AlarmSetScreen(): React.JSX.Element {
         }
       };
       fetchMode();
-      if(isAlarmEnabled){
-        scheduleAlarm()
-      }
 
       return () => {
         setIsAlarmSetOpen(false)
@@ -174,6 +81,60 @@ function AlarmSetScreen(): React.JSX.Element {
       };
     }, [dispatch])
   );
+
+  /////////////////////////////////
+
+  useFocusEffect(
+    React.useCallback(() => {
+      // Do something when the screen is focused
+      dispatch(selectTab(3))// bottomtab의 표시
+      return () => {
+        setIsAlarmSetOpen(false)
+        // Do something when the screen is unfocused
+        // Useful for cleanup functions
+      };
+    }, [])
+  );
+
+  ///// push 알람을 위한 notifee코드
+  async function onDisplayNotification() {
+    // Request permissions (required for iOS)
+    await notifee.requestPermission()
+
+    // Create a channel (required for Android)
+    const channelId = await notifee.createChannel({
+      id: 'alarm_channel',
+      name: 'Alarm Channel',
+    });
+
+    // Required for iOS
+    // See https://notifee.app/react-native/docs/ios/permissions
+    await notifee.requestPermission();
+
+    // const notificationId = await notifee.displayNotification({
+    //   id: '123',
+    //   title: 'Notification Title',
+    //   body: 'Main body content of the notification',
+    //   android: {
+    //     channelId,
+    //   },
+    // });
+
+    // Display a notification
+    await notifee.displayNotification({
+      title: '일기 작성 시간!',  //Notification Title
+      body: '하루일기에서 오늘의 이야기를 작성해보세요.', //Main body content of the notification
+      android: {
+        channelId,
+        // smallIcon: 'name-of-a-small-icon', // optional, defaults to 'ic_launcher'.
+        // pressAction is needed if you want the notification to open the app when pressed
+        pressAction: {
+          id: 'alarm_channel',
+        },
+      },
+    });
+  }
+  ////
 
   return (
     <SafeAreaView style={[styles.container, styles.pdvr2, styles.pdhr2]}>
@@ -222,12 +183,14 @@ function AlarmSetScreen(): React.JSX.Element {
           </Text>
         </TouchableOpacity>
         {/* <Text>selected: {date.getTime()}</Text> */}
-        {/* <View>
+        <View>
           <Button title="Display Notification" onPress={() => onDisplayNotification()} />
-        </View> */}
+        </View>
       </TouchableOpacity>
 
       {isAlarmSetOpen && (
+        // <AlarmSetModal isAlarmSetOpen={isAlarmSetOpen} handleModalClose={handleModalClose}/>
+        // <AlarmSetModal handleModalClose={handleModalClose}/>
         <DateTimePicker
           // testID="dateTimePicker"
           value={date}
